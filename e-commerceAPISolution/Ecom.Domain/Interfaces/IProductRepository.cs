7988ac56f6ca20711da07f20830f5b798876522f
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2025-12-22 17:12:45 +0200

    feat(domain): add product repository contract

---
+++ e-commerceAPISolution/Ecom.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using Ecom.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Domain.Interfaces
+{
+	public interface IProductRepository
+	{
+		Task AddProductAsync(Product product); //we didnt return the product because this is add thing in repo ,return in Service
+
+		Task<IReadOnlyList<Product>> GetAllProductsAsync(); //we returned because its a read things so its normal to return
+
+		Task<Product?> GetProductByIdAsync(Guid id);//we returned because its a read things so its normal to return
+
+	}
+}
