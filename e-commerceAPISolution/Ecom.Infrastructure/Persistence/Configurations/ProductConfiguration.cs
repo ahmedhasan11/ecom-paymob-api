3fa5436652a85cf6992075c248941cd16deeacc1
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-01-30 00:20:09 +0200

    feat(infrastructure): setup persistence with DbContext and Product configuration

---
+++ e-commerceAPISolution/Ecom.Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Ecom.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Persistence.Configurations
+{
+	public class ProductConfiguration : IEntityTypeConfiguration<Product>
+	{
+		public void Configure(EntityTypeBuilder<Product> builder)
+		{
+			// ID : Primary Key 
+			//Name : Required , MaxLength(100)
+			//Price : Required , Decimal , Numeric
+			//Description : MaxLength(200)
+			builder.HasKey(p => p.Id);
+			builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
+			builder.Property(p => p.Price).IsRequired().HasPrecision(18, 2);
+			builder.Property(p => p.Description).HasMaxLength(200);
+
+		}
+	}
+}
