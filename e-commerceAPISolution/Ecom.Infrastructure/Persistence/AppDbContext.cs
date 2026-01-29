ee6d37f18c61b6fcdc54c50005a6cf745e863e1f
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-01-30 00:20:09 +0200

    feat(infrastructure): setup persistence with DbContext and Product configuration

---
+++ e-commerceAPISolution/Ecom.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Ecom.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Persistence
+{
+	public class AppDbContext : IdentityDbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+		{
+		}
+		public DbSet<Product> Products { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+
+			builder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+			/*يدوّر على أي كلاس بيطبّق إعدادات كيانات (زي ProductConfiguration)
+يشغّل ميثود Configure بتاعته تلقائي*/ 
+		}
+	}
+}
---
+++ e-commerceAPISolution/Ecom.Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using Ecom.Domain.Entities;
+using Ecom.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Persistence.Repositories
+{
+	public class ProductRepository : IProductRepository
+	{
+		private readonly AppDbContext _dbContext;
+		public ProductRepository( AppDbContext context) 
+		{
+			_dbContext = context;
+		}
+		public async Task AddProductAsync(Product product)
+		{
+			 await _dbContext.Products.AddAsync(product);
+			 //await _dbContext.SaveChangesAsync();
+		}
+
+		public async Task<IReadOnlyList<Product>> GetAllProductsAsync()
+		{
+			return await _dbContext.Products.ToListAsync();
+		}
+
+		public async Task<Product?> GetProductByIdAsync(Guid id)
+		{
+			return await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == id);
+		}
+	}
+}
