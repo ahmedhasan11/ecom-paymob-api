1f35939fefaf8c22e92114559fde85a66891a1cc
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-01-30 18:17:13 +0200

    feat(application): integrate Unit of Work and configure DI via extension methods

---
+++ e-commerceAPISolution/Ecom.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using Ecom.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Persistence
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly AppDbContext _dbContext;
+		public UnitOfWork(AppDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+		{
+			return await _dbContext.SaveChangesAsync(cancellationToken);
+		}
+	}
+}
--- e-commerceAPISolution/e-commerceAPI/Program.cs
+++ e-commerceAPISolution/e-commerceAPI/Program.cs
@@ -1,4 +1,4 @@
-
+using Ecom.Infrastructure.Dependency_Injection;
 namespace e_commerceAPI
 {
     public class Program
@@ -7,10 +7,9 @@ namespace e_commerceAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            builder.Services.AddInfrastructure(builder.Configuration);
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
