14a37ac34b53dacbed51fd796d4d2d7c7c9fe99f
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-02-05 16:31:07 +0200

    feat(products): add filtering (search, price range) with pagination support

---
+++ e-commerceAPISolution/Ecom.Application/Common/Filtering/ProductQueryParams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Application.Common.Filtering
+{
+	public class ProductQueryParams
+	{
+		//Paging
+		public int pageNumber { get; set; } = 1;
+		public int pageSize { get; set; } = 10;
+
+		//Filtering
+		public string? search { get; set; }
+		public decimal? MinPrice { get; set; }
+		public decimal? MaxPrice { get; set; }
+	}
+}
