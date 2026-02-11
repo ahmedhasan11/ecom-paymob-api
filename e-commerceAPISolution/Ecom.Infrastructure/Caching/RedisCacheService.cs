a927c3708b45d47f6119bba5f998f55d95d37123
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-02-11 15:39:20 +0200

    feat: implement Redis distributed caching with TTL for product queries

---
+++ e-commerceAPISolution/Ecom.Infrastructure/Caching/RedisCacheService.cs
@@ -0,0 +1,37 @@
+﻿
+using Ecom.Application.Interfaces;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Caching
+{
+	public class RedisCacheService : ICacheService
+	{
+		private readonly IDatabase _db;
+		public RedisCacheService(RedisConnectionFactory connectionFactory)
+		{
+			_db = connectionFactory.GetDatabase();
+		}
+
+		public async Task<string?> GetAsync(string key)
+		{
+			return await _db.StringGetAsync(key);
+		}
+
+		public async Task SetAsync(string key , string value , TimeSpan expiry)
+		{
+			 await _db.StringSetAsync(key, value, expiry);
+			//Redis يمسحه تلقائيًا بعد المدة (TTL)
+		}
+
+		public async Task RemoveAsync(string key)
+		{
+			await _db.KeyDeleteAsync(key);
+			//دي هنستخدمها لما يحصل Update أو Delete لمنتج
+		}
+	}
+}
