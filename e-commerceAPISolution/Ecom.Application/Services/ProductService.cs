871e32953800b0062634d46ea24bed36c308c8ac
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-02-11 19:20:28 +0200

    feat(serilog): integrate Serilog and add structured logging for ProductService and global exceptions

--- e-commerceAPISolution/Ecom.Application/Services/ProductService.cs
+++ e-commerceAPISolution/Ecom.Application/Services/ProductService.cs
@@ -7,6 +7,7 @@ using Ecom.Domain.Common.Queries;
 using Ecom.Domain.Entities;
 using Ecom.Domain.Interfaces;
 using Ecom.Domain.ValueObjects;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
@@ -25,29 +26,41 @@ namespace Ecom.Application.Services
 		private readonly IProductRepository _productRepository;
 		private readonly IUnitOfWork _unitOfWork;
 		private readonly ICacheService _cacheService;
-		public ProductService(IProductRepository productRepository, IUnitOfWork unitOfWork, ICacheService cacheService)
+		private readonly ILogger<ProductService> _logger;
+		public ProductService(IProductRepository productRepository, IUnitOfWork unitOfWork, ICacheService cacheService, ILogger<ProductService> logger)
 		{
 			_productRepository = productRepository;
 			_unitOfWork = unitOfWork;
 			_cacheService = cacheService;
+			_logger = logger;
 		}
 		public async Task<ProductDto> AddProductAsync(RequestAddProductDto requestAddProductDto)
 		{
 			if (requestAddProductDto == null)
 			{
+				_logger.LogWarning("AddProduct operation received a null request payload");
+
+
 				throw new ArgumentNullException(nameof(requestAddProductDto));
 			}
+
+			_logger.LogInformation("Starting product creation with Name={Name} and Price={Price}",
+			requestAddProductDto.Name, requestAddProductDto.Price);
 			Product product= new Product(requestAddProductDto.Price, requestAddProductDto.Name);
 			product.ImageUrl = requestAddProductDto.ImageUrl;
 			product.Description = requestAddProductDto.Description;
 
+			#region notes
 			//product.Id= Guid.NewGuid();
 			//product.Name= requestAddProductDto.Name;
 			//product.Price= Money.From( requestAddProductDto.Price);
 			//أي فلوس تدخل الدومين لازم تعدّي على Money.From عشان أتأكد إنها فلوس صح، مش رقم وخلاص
-
+			//_logger.LogInformation("going inside Repo"); 
+			#endregion
 			await _productRepository.AddProductAsync(product);
 			await _unitOfWork.SaveChangesAsync();
+
+			_logger.LogInformation("Product created successfully with Id={ProductId}", product.Id);
 
 			return new ProductDto() { Id = product.Id, Name = product.Name, Price = product.Price.Amount, CreatedAt = product.CreatedAt , Description= product.Description, ImageUrl= product.ImageUrl };
 		}
@@ -150,6 +163,9 @@ namespace Ecom.Application.Services
 			}
 			#endregion
 
+			_logger.LogInformation("Fetching products with filters: Search={Search}, MinPrice={Min}, MaxPrice={Max}, Page={Page}, Size={Size}, SortBy={SortBy}, SortOrder={SortOrder}",
+			search, minPrice, maxPrice, pageNumber, pageSize,sortBy,sortOrder);
+
 			#region Redis Caching
 			var key = $"products:search={search}" +
 		$":min={minPrice}:max={maxPrice}:sort={sortBy}:order={sortOrder}:page={pageNumber}:size={pageSize}";
@@ -160,11 +176,12 @@ namespace Ecom.Application.Services
 				PagedResult<ProductDto>? result = JsonSerializer.Deserialize<PagedResult<ProductDto>>(cachedJson);
 				if (result != null)
 				{
+					_logger.LogInformation("Cache HIT for products list. Key={CacheKey}", key);
 					return result;
 				}
-			} 
+			}
 			#endregion
-
+			_logger.LogInformation("Cache MISS for products list. Key={CacheKey}. Fetching from database...", key);
 			ProductQueryOptions productQueryOptions = new ProductQueryOptions()
 			{
 			 search= search,
@@ -180,6 +197,8 @@ namespace Ecom.Application.Services
 
 			IReadOnlyList<Product> products= await _productRepository.GetProductsAsync(productQueryOptions);
 
+			_logger.LogInformation("Fetched {Count} products from database", products.Count);
+
 			IReadOnlyList<ProductDto> productDtos= products.Select(p => new ProductDto { Id = p.Id, Name = p.Name, Price = p.Price.Amount, CreatedAt = p.CreatedAt, ImageUrl= p.ImageUrl, Description=p.Description }).ToList();
 
 			PagedResult<ProductDto> pagedResult = new PagedResult<ProductDto>() 
@@ -193,14 +212,20 @@ namespace Ecom.Application.Services
 
 			string JsonToCache = JsonSerializer.Serialize(pagedResult);
 			await _cacheService.SetAsync(key, JsonToCache, TimeSpan.FromMinutes(1));
+			_logger.LogInformation("Products list cached successfully. Key={CacheKey}", key);
+
 			return pagedResult;
 		}	 
 		public async Task<ProductDto?> GetProductByIdAsync(Guid id)
 		{
 			if (id == Guid.Empty)
 			{
+				_logger.LogWarning("GetProductById received an empty ProductId");
 				throw new ArgumentException("id cannot be empty", nameof(id));
 			}
+
+			_logger.LogInformation(
+				"Starting GetProductById operation. ProductId={ProductId}", id);
 
 			#region RedisCaching
 			var key = $"products:{id}";
@@ -210,10 +235,17 @@ namespace Ecom.Application.Services
 				var result = JsonSerializer.Deserialize<ProductDto>(cachedJson);
 				if (result != null)
 				{
+					_logger.LogInformation(
+						"Cache HIT for product. ProductId={ProductId}, CacheKey={CacheKey}",
+						id, key);
+
 					return result;
 				}
-			} 
+			}
 			#endregion
+			_logger.LogInformation(
+				"Cache MISS for product. ProductId={ProductId}. Fetching from database",
+				id);
 
 			Product? product = await _productRepository.GetProductByIdAsync(id);
 			if (product == null)
@@ -222,9 +254,12 @@ namespace Ecom.Application.Services
 				//notice: this is not an exception , exception is for programmateic error
 				//but the status we are in is just a not found
 				//return null;
+
+				_logger.LogWarning("Product not found. ProductId={ProductId}", id);
+
 				throw new NotFoundException($"Product with {id} was not found ");
 			}
-
+			
 			var productdto = new ProductDto()
 			{
 				Id = product.Id,
@@ -236,25 +271,36 @@ namespace Ecom.Application.Services
 			};
 			var jsontocache = JsonSerializer.Serialize(productdto);
 			await _cacheService.SetAsync(key,jsontocache,TimeSpan.FromMinutes(2));
+			_logger.LogInformation(
+				"Product cached successfully. ProductId={ProductId}, CacheKey={CacheKey}",
+				id, key);
 
 			return productdto;
 
 		}
 		public async Task<ProductDto?> UpdateProductAsync(Guid id, RequestUpdateProductDto requestupdateProductDto)
 		{
+
 			if (id==Guid.Empty)
 			{
+				_logger.LogWarning("UpdateProduct received an empty ProductId");
+
 				throw new ArgumentException("ID cannot be empty", nameof(id));
 			}
 
 			if (requestupdateProductDto==null)
 			{
+				_logger.LogWarning("UpdateProduct request payload was null. ProductId={ProductId}", id);
+
 				throw new ArgumentNullException("dto is null", nameof(requestupdateProductDto));
 			}
+			_logger.LogInformation(
+			"Starting UpdateProduct operation. ProductId={ProductId}", id);
 
 			Product? product = await _productRepository.GetProductByIdAsync(id);
 			if (product==null)
 			{
+				_logger.LogWarning("Product not found for update. ProductId={ProductId}", id);
 				throw new NotFoundException($"Product with {id} was not found ");
 			}
 			if (requestupdateProductDto.Name is not null)
@@ -274,6 +320,8 @@ namespace Ecom.Application.Services
 				product.Description = requestupdateProductDto.Description;
 			}
 			await _unitOfWork.SaveChangesAsync();
+			_logger.LogInformation(
+			"Product updated successfully. ProductId={ProductId}", product.Id);
 
 			return new ProductDto() { Id= product.Id, Name= product.Name ,Price= product.Price.Amount, CreatedAt = product.CreatedAt , ImageUrl= product.ImageUrl , Description= product.Description};
 
@@ -282,15 +330,26 @@ namespace Ecom.Application.Services
 		{
 			if (id==Guid.Empty)
 			{
+				_logger.LogWarning("DeleteProduct received an empty ProductId");
 				throw new ArgumentException("ID cannot be empty", nameof(id));
 			}
+			_logger.LogInformation(
+			"Starting DeleteProduct operation. ProductId={ProductId}", id);
 			Product? product = await _productRepository.GetProductByIdAsync(id);
 			if (product==null)
 			{
+				_logger.LogWarning("Product not found for deletion. ProductId={ProductId}", id);
+
 				throw new NotFoundException($"Product with {id} was not found ");
 			}
+			_logger.LogInformation(
+				"Deleting product. ProductId={ProductId}, ProductName={ProductName}",
+				product.Id, product.Name);
+
 			await _productRepository.DeleteProductAsync(product);
 			await _unitOfWork.SaveChangesAsync();
+			_logger.LogInformation("Product deleted successfully. ProductId={ProductId}", id);
+
 			return true;
 		}
 	}
