69a149d403c0ce21fd336a02eb97df34912ff49c
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-02-13 20:59:27 +0200

    feat(fluent-validation): integrate FluentValidation for add and update product DTOs

--- e-commerceAPISolution/Ecom.Application/DTOs/Products/RequestUpdateProductDto.cs
+++ e-commerceAPISolution/Ecom.Application/DTOs/Products/RequestUpdateProductDto.cs
@@ -9,13 +9,9 @@ namespace Ecom.Application.DTOs.Products
 {
 	public class RequestUpdateProductDto
 	{
-		[StringLength(100)]
 		public string? Name { get; set; }
-		[Range(0, 10000)]
 		public decimal? Price { get; set; }
-
 		public string? ImageUrl { get; set; }
-
 		public string? Description { get; set; }
 	}
 }
