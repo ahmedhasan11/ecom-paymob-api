fb0b9fa89042b6a2b993d5b4211cbfcbbf1dd57e
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-03-14 03:03:23 +0200

    feat(payment): add domain methods and lifecycle guards for Payment entity

--- e-commerceAPISolution/Ecom.Domain/Entities/Payment.cs
+++ e-commerceAPISolution/Ecom.Domain/Entities/Payment.cs
@@ -24,7 +24,78 @@ namespace Ecom.Domain.Entities
 		public DateTime? FailedAt { get; private set; }
 		private Payment() { }
 
+		public static Payment Create(Guid orderId, decimal amount, string currency)
+		{
+			if (orderId==Guid.Empty)
+			{
+				throw new ArgumentException("orderId cannot be empty.");
+			}
+			if (string.IsNullOrWhiteSpace(currency))
+			{
+				throw new ArgumentException("currency cannot be null");
+			}
+			if (amount <= 0)
+			{
+				throw new ArgumentException("amount can't be less than or equal 0");
+			}
+			var payment = new Payment() 
+			{
+				Id= Guid.NewGuid(),
+				Status= PaymentStatusEnum.Pending,
+				OrderId= orderId,
+				Amount=amount,
+				Currency=currency,
+				Gateway=GatewayEnum.Paymob		
+			};
+			return payment;
+		}
 
+		public void SetPaymobOrderId(long paymobOrderId)
+		{
+			if (paymobOrderId <= 0)
+			{
+				throw new ArgumentException("paymobOrderId is not valid");
+			}
+			if (PaymobOrderId.HasValue)
+			{
+				throw new InvalidOperationException();
+			}
+			PaymobOrderId = paymobOrderId;
+		}
+
+		public void MarkAsSucceeded(long transactionId)
+		{
+			if (transactionId <= 0)
+			{
+				throw new ArgumentException("paymobTransactionId is not valid");
+			}
+			if (Status != PaymentStatusEnum.Pending)
+			{
+				throw new InvalidOperationException();
+			}
+			Status = PaymentStatusEnum.Succeeded;
+			PaidAt = DateTime.UtcNow;
+			PaymobTransactionId = transactionId;
+		}
+		public void MarkAsFailed(long? transactionId)
+		{
+			if (Status != PaymentStatusEnum.Pending)
+			{
+				throw new InvalidOperationException();
+			}
+			Status = PaymentStatusEnum.Failed;
+			FailedAt= DateTime.UtcNow;
+			PaymobTransactionId= transactionId;
+		}
+
+		public void MarkAsCancelled()
+		{
+			if (Status != PaymentStatusEnum.Pending)
+			{
+				throw new InvalidOperationException();
+			}
+			Status = PaymentStatusEnum.Cancelled;
+		}
 
 	}
 }
