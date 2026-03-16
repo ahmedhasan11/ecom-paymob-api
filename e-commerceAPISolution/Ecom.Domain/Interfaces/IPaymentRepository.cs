beaf4c11f8769418395c3987735c3b7ac03a776d
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-03-16 22:22:12 +0200

    feat(payment): add IPaymentRepository contract & implement AddAsync in PaymentRepository

---
+++ e-commerceAPISolution/Ecom.Domain/Interfaces/IPaymentRepository.cs
@@ -0,0 +1,22 @@
+﻿using Ecom.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Domain.Interfaces
+{
+	public interface IPaymentRepository
+	{
+		Task CreatePaymentAsync(Payment payment, CancellationToken cancellationToken);
+
+		Task<Payment?> GetPendingPaymentByOrderId(Guid orderId, CancellationToken cancellationToken);
+
+		Task<Payment?> GetPaymentByPaymobOrderIdAsync(long paymobOrderId, CancellationToken cancellationToken);
+
+		Task<Payment?> GetPaymentByPaymentId(Guid paymentId, CancellationToken cancellationToken);
+
+
+	}
+}
---
+++ e-commerceAPISolution/Ecom.Infrastructure/Persistence/Repositories/PaymentRepository.cs
@@ -0,0 +1,25 @@
+﻿using Ecom.Domain.Entities;
+using Ecom.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Infrastructure.Persistence.Repositories
+{
+	public class PaymentRepository : IPaymentRepository
+	{
+		private readonly AppDbContext _db;
+		public PaymentRepository(AppDbContext db)
+		{
+			_db = db;
+		}
+
+		public async Task CreatePaymentAsync(Payment payment, CancellationToken cancellationToken)
+		{
+			await _db.Payments.AddAsync(payment, cancellationToken);
+		}
+	}
+}
