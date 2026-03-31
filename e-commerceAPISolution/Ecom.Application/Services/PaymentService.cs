cf09a62b8a0608b887c79d1c94315cc0dc76d481
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-03-31 21:03:58 +0200

    feat(payment): add PaymentService skeleton and interface integration
    - Implement PaymentService class in Application layer
    - Inject required dependencies (IPaymentRepository, IPaymentGateway, IOrderRepository, IUnitOfWork)
    - Add CreatePaymentSessionAsync method signature
    - Prepare structure for payment session workflow

---
+++ e-commerceAPISolution/Ecom.Application/Services/PaymentService.cs
@@ -0,0 +1,30 @@
+﻿using Ecom.Application.DTOs.Payments;
+using Ecom.Application.Interfaces;
+using Ecom.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Application.Services
+{
+	public class PaymentService : IPaymentService
+	{
+		private readonly IPaymentRepository _paymentRepository;
+		private readonly IPaymentGateway _paymentGateway;
+		private readonly IUnitOfWork _unitOfWork;
+		private readonly IOrderRepository _orderRepository;
+		public PaymentService(IPaymentRepository paymentRepository, IPaymentGateway paymentGateway, IUnitOfWork unitOfWork, IOrderRepository orderRepository)
+		{
+			_paymentRepository = paymentRepository;
+			_paymentGateway = paymentGateway;
+			_unitOfWork = unitOfWork;
+			_orderRepository = orderRepository;
+		}
+		public async Task<PaymentSessionResponse> CreatePaymentSessionAsync(Guid orderId, CancellationToken cancellationToken)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
