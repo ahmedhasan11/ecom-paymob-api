24f882f64fdb6f3d18e94436de96ec62b1d9da79
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-03-31 20:58:41 +0200

    IPaymentServic interface

---
+++ e-commerceAPISolution/Ecom.Application/Interfaces/IPaymentService.cs
@@ -0,0 +1,14 @@
+﻿using Ecom.Application.DTOs.Payments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Application.Interfaces
+{
+	public interface IPaymentService
+	{
+		Task<PaymentSessionResponse> CreatePaymentSessionAsync(Guid orderId, CancellationToken cancellationToken);
+	}
+}
