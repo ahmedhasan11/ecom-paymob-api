5e93cd5dcee0c1d3dbd04724c3d1ce0b936a0791
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-04-22 18:48:39 +0200

    IPaymentExpirationService interface method && GetExpiredPendingPayments in PaymentRepo with batching && handle if a payment is stuck more than 10 minutes

---
+++ e-commerceAPISolution/Ecom.Application/Interfaces/IPaymentExpirationService.cs
@@ -0,0 +1,14 @@
+﻿using Ecom.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecom.Application.Interfaces
+{
+	public interface IPaymentExpirationService
+	{
+		 Task ExpirePaymentsAsync(CancellationToken cancellationToken);
+	}
+}
--- e-commerceAPISolution/Ecom.Infrastructure/Persistence/Repositories/PaymentRepository.cs
+++ e-commerceAPISolution/Ecom.Infrastructure/Persistence/Repositories/PaymentRepository.cs
@@ -27,11 +27,26 @@ namespace Ecom.Infrastructure.Persistence.Repositories
 		}
 		public async Task<Payment?> GetPendingPaymentByOrderId(Guid orderId, CancellationToken cancellationToken)
 		{
-			return await _db.Payments.AsNoTracking().FirstOrDefaultAsync(p => p.OrderId == orderId && p.Status == PaymentStatusEnum.Pending,cancellationToken);
+			return await _db.Payments.FirstOrDefaultAsync(p => p.OrderId == orderId && p.Status == PaymentStatusEnum.Pending,cancellationToken);
 		}
 		public async Task<Payment?> GetPaymentByPaymobOrderIdAsync(long paymobOrderId, CancellationToken cancellationToken)
 		{
 			return await _db.Payments.FirstOrDefaultAsync(p => p.PaymobOrderId == paymobOrderId, cancellationToken);
+		}
+
+		public async Task<List<Payment>> GetExpiredPendingPaymentsAsync(CancellationToken cancellationToken)
+		{
+			var now = DateTime.UtcNow;
+			var tenMinutesAgo = now.AddMinutes(-10);
+			return await _db.Payments
+				.Where(p => p.Status == PaymentStatusEnum.Pending &&
+				(
+					(p.ExpiresAt.HasValue && p.ExpiresAt <= now) ||
+					(!p.ExpiresAt.HasValue && p.CreatedAt <= tenMinutesAgo)
+				))
+				.OrderBy(p=>p.CreatedAt)
+				.Take(100)
+				.ToListAsync(cancellationToken);
 		}
 	}
 }
